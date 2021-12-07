3d7169e6c9a8bf604282956d6ac666be27dd8abb
Elisey Gubarev <79366573+Crowfoody@users.noreply.github.com>
2021-12-07 19:27:29 +0300

    Update: color pick

--- script.js
+++ script.js
@@ -4,3 +4,14 @@ document.querySelector('.navigation__mobile-menu').classList.toggle('active');
 document.querySelector('.navigation__desktop-menu').classList.toggle('active');
 document.querySelector('body').classList.toggle('lock');
 }
+let colorItems = document.querySelectorAll('.top-select__item');
+
+for (let i = colorItems.length - 1; i >= 0; i--) {
+	colorItems[i].onclick = function() {
+		for (let i = colorItems.length - 1; i >= 0; i--) {
+				colorItems[i].classList.remove('selected-color');
+			}
+			this.classList.add('selected-color');
+		}
+	
+}
